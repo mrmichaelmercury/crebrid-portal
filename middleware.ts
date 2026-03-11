d4adf2487f73e72e70da9f1f2d5cf1a0956d798b
mrmichaelmercury <themidastouchtradinggroup@gmail.com>
2026-03-11 16:27:32 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,23 @@
 import NextAuth from "next-auth";
-    import { authConfig } from "./auth.config";
-    import { NextResponse } from "next/server";
-    
-    export default NextAuth(authConfig).auth((req) => {
-      const { pathname } = req.nextUrl;
-      const isLoggedIn = !!req.auth;
-      const isPublicPath = pathname.startsWith("/login");
-      const isApiAuth = pathname.startsWith("/api/auth");
-    
-      if (isApiAuth) return NextResponse.next();
-      if (isPublicPath && isLoggedIn) {
-        return NextResponse.redirect(new URL("/dashboard", req.url));
-      }
-      if (!isPublicPath && !isLoggedIn && !pathname.startsWith("/api")) {
-        return NextResponse.redirect(new URL("/login", req.url));
-      }
-      return NextResponse.next();
-    });
-    
-    export const config = {
-      matcher: ["/((?!_next/static|_next/image|favicon.ico|manifest.json|icon-).*)"],
-    };
+import { authConfig } from "./auth.config";
+import { NextResponse } from "next/server";
+
+export default NextAuth(authConfig).auth((req) => {
+  const { pathname } = req.nextUrl;
+  const isLoggedIn = !!req.auth;
+  const isPublicPath = pathname.startsWith("/login");
+  const isApiAuth = pathname.startsWith("/api/auth");
+
+  if (isApiAuth) return NextResponse.next();
+  if (isPublicPath && isLoggedIn) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+  if (!isPublicPath && !isLoggedIn && !pathname.startsWith("/api")) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|manifest.json|icon-).*)"],
+};
